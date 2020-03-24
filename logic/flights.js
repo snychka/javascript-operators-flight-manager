1527eecb653c8302a981699fa2f38d35768bbf72
Stefan Nychka <stefannychka@gmail.com>
2020-03-24 12:44:42 -0600

    causes uncaught error which would break online

--- logic/flights.js
+++ logic/flights.js
@@ -1,4 +1,61 @@
+
+
 function Flights() {
+
+    //function calculateNumberOfFlights (p, fc) {
+    let calculateNumberOfFlights = (p, fc) => {
+
+        if (!Number.isInteger(p) || p < 0) {
+            throw new Error("The number of passengers must be a positive integer value");
+        }
+        if (!Number.isInteger(fc) || fc < 0) {
+            throw new Error("The capacity of the flight must be a positive integer value");
+        }
+
+        let flights = p % fc === 0 ?
+            Math.floor(p/fc) : 
+            Math.floor(p/fc) + 1;
+        return flights;
+    }
+
+    // a bit redundant, namely else if, but should work
+    function checkAircraftRevision (dl, da) {
+        let sum = da.reduce((s, c) => s+c, 0);
+        if (sum < dl/2) {
+            //return "3 months";
+            return "The revision needs to be done within the next 3 months";
+        } else if (sum >= dl/2 && sum < 0.75*dl) {
+            //return "2 months";
+            return "The revision needs to be done within the next 2 months";
+        } else if (sum >= 0.75*dl && sum <= dl) {
+            //return "1 month";
+            //return "The revision needs to be done within the next 1 month";
+            return "The revision needs to be done within the next month";
+        } else {
+            throw new Error("exceeded distance limit");
+        }
+    }
+
+    /*
+    function checkAircraftRevision(distanceLimit, distancesArray) {
+        let totalDistance = 0;
+        let distance;
+        for(distance of distancesArray) {
+            totalDistance += distance;
+        }
+
+        if (totalDistance <= distanceLimit/2) {
+            return "The revision needs to be done within the next 3 months";
+        } else if (totalDistance <= 3 * distanceLimit/4) {
+            return "The revision needs to be done within the next 2 months";
+        } else {
+            return "The revision needs to be done within the next month";
+        }
+     }
+     */
+
+    return {calculateNumberOfFlights, checkAircraftRevision};
 
 }
 
+module.exports = Flights();
--- logic/passengers.js
+++ logic/passengers.js
@@ -1,4 +1,23 @@
 function Passengers() {
+/*
+In passengers.js, write a checkFlightCapacity() function that gets as parameters the flight capacity and and array of passengers numbers. Calculate the total number of passengers by adding each number of passengers from the array. If the total number of passengers is less that the capacity of the flight, return it. If there are more passengers than the flight capacity, you should throw an error.
+
+Export the checkFlightCapacity() function and the Passengers module itself.
+
+Check to see that the functionality "Check the flight capacity" from the front end is running fine./*
+
+    function checkFlightCapacity(fc, pn) {
+        let sum = pn.reduce((s, c) => s+c, 0);
+        if (sum < fc) {
+            return sum;
+        } else {
+            throw Error("Too many passengers");
+        }
+    }
+
+    return {checkFlightCapacity};
 
 }
+
+module.exports = Passengers();
 
