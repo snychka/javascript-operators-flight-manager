4be8d2fe8e5cd94437743b4e5c6a6310709bdd57
snychka <stefannychka@gmail.com>
2020-03-24 19:43:54 -0600

    done module2, although no use strict, as my code is likely horrible :)

--- logic/util.js
+++ logic/util.js
@@ -1,4 +1,25 @@
 function Util() {
 
+  function calculateTotalDistributedPassengers(
+    {
+      vipPassengersWithBusinessSeats, vipPassengersWithEconomySeats,
+      regularPassengersWithBusinessSeats, regularPassengersWithEconomySeats
+    }
+  ) {
+    let x = 
+      vipPassengersWithBusinessSeats + vipPassengersWithEconomySeats +
+      regularPassengersWithBusinessSeats + regularPassengersWithEconomySeats;
+
+    //console.log('stef: ' + x);
+    return x;
+  }
+
+  function calculateTotalNumberOfPassengers(p) {
+    return p.reduce((s,c) => s+c, 0);
+  }
+
+  return {calculateTotalDistributedPassengers, calculateTotalNumberOfPassengers};
+
 }
 
+module.exports = Util();
